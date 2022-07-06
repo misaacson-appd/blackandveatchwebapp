2480ec693b16e95e599dace5ae5959dcb7282027
Michael Isaacson <michael.isaacson@appdynamics.com>
2022-07-06 14:39:19 -0500

    Compiles, but doesn't run: it can't find appd

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// eslint-disable-next-line
+require("appdynamics").profile({
+  controllerHostName: "se-lab.saas.appdynamics.com",
+  controllerPort: 443,
+  controllerSslEnabled: true,
+  accountName: "se-lab",
+  accountAccessKey: "hj6a4d7h2cuq",
+  applicationName: "z_moi_blackandveatch",
+  tierName: "web",
+  nodeName: "vue",
+});
+
 createApp(App).use(store).use(router).mount("#app");
