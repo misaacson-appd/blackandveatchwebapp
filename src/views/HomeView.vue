d3af17e40e6d721f35c97901878282ac744db10a
Michael Isaacson <michael.isaacson@appdynamics.com>
2022-07-06 12:51:06 -0500

    Linted

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="https://blackandveatchstorage.blob.core.windows.net/www/BlackAndVeatch.jpeg" />
+    <img
+      alt="Vue logo"
+      src="https://blackandveatchstorage.blob.core.windows.net/www/BlackAndVeatch.jpeg"
+    />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
     <p>blackandveatchfrontdoor-gbesd3hdc0hhdcdt.z01.azurefd.net</p>
     <p>Origin: blackandveatchstorage.blob.core.windows.net</p>
